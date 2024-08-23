4caba14b55e89b5bf51e0be1c29fac1d13e45d70
jesusdefelipe17 <jesusdefelipe17@gmail.com>
2024-08-23 12:45:37 +0200

    getPelicula endpoint

---
+++ getPelicula.js
@@ -0,0 +1,64 @@
+const puppeteer = require("puppeteer");
+require("dotenv").config();
+
+const getPelicula = async (req, res) => {
+  const nombrePelicula = req.params.nombrePelicula;
+  const url = `https://peliculas10.pro/pelicula/${nombrePelicula}/`;
+
+  const browser = await puppeteer.launch({
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
+
+  try {
+    const page = await browser.newPage();
+    await page.goto(url, { waitUntil: 'networkidle2' });
+
+    const frameElement = await page.waitForSelector('#dooplay_player_content iframe');
+    const frame = await frameElement.contentFrame();
+
+    if (frame) {
+      const allLinks = await frame.$$eval('div.OD_1.REactiv li', items =>
+        items.map(item => {
+          const onclickValue = item.getAttribute('onclick');
+          const urlMatch = onclickValue && onclickValue.match(/go_to_playerVast\('([^']+)'/);
+          return urlMatch ? urlMatch[1] : null;
+        }).filter(url => url !== null)
+      );
+
+      const doodstreamLinks = allLinks.filter(url => url.includes('doodstream'));
+      const streamwishLinks = allLinks.filter(url => url.includes('streamwish'));
+      const filteredLinks = doodstreamLinks.length > 0 ? doodstreamLinks : streamwishLinks;
+
+      const [sinopsis, posterSrc, rating] = await Promise.all([
+        page.$eval('div[itemprop="description"] p', p => p.textContent.trim()),
+        page.$eval('div.poster img[itemprop="image"]', img => img.src.replace(/w185/, 'w500')),
+        page.$eval('span.valor b#repimdb strong', strong => strong.textContent.trim())
+      ]);
+
+      res.json({
+        links: filteredLinks,
+        descripcion: sinopsis,
+        poster: posterSrc,
+        calificacion: rating,
+      });
+    } else {
+      res.status(404).json({ error: 'No se encontró el iframe.' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Hubo un error al obtener los datos.' });
+  } finally {
+    await browser.close();
+  }
+};
+
+module.exports = { getPelicula };
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { ultimasPeliculas } = require("./ultimasPeliculas");
+const { getPelicula } = require("./getPelicula");
 const app = express();
 
 const PORT = process.env.PORT || 4000;
@@ -8,8 +9,12 @@ app.get("/ultimasPeliculas", (req, res) => {
     ultimasPeliculas(res);
 });
 
+app.get("/getPelicula/:nombrePelicula", (req, res) => {
+    getPelicula(req, res);
+});
+
 app.get("/", (req, res) => {
-  res.send("Render Puppeteer server is up and running!");
+  res.send("servidor levantado de Peliculas");
 });
 
 app.listen(PORT, () => {
