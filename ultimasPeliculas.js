7091cf3cafc02699af3197c731abe1e703b831f8
jesusdefelipe17 <jesusdefelipe17@gmail.com>
2024-09-17 00:40:45 +0200

    fixs

--- ultimasPeliculas.js
+++ ultimasPeliculas.js
@@ -1,69 +1,51 @@
 const puppeteer = require("puppeteer");
 require("dotenv").config();
 
-// Cachear datos durante un tiempo definido
-let cachedData = null;
-let lastCacheTime = 0;
-const CACHE_DURATION_MS = 1000 * 60 * 60 * 24; // Cache por 24 horas
-
 const ultimasPeliculas = async (res) => {
-  const currentTime = Date.now();
-  if (cachedData && (currentTime - lastCacheTime) < CACHE_DURATION_MS) {
-    return res.json({ peliculas: cachedData });
-  }
-
-  let browser = null;
+  const browser = await puppeteer.launch({
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
   try {
-    browser = await puppeteer.launch({
-      headless: "new",
-      args: [
-        "--disable-setuid-sandbox",
-        "--no-sandbox",
-        "--single-process",
-        "--no-zygote",
-      ],
-      executablePath:
-        process.env.NODE_ENV === "production"
-          ? process.env.PUPPETEER_EXECUTABLE_PATH
-          : puppeteer.executablePath(),
-    });
-
     const page = await browser.newPage();
-
     const url = 'https://peliculas10.pro';
 
     await page.goto(url, { waitUntil: 'networkidle2' });
-    
-    // Extraer los datos esenciales
+
     const peliculas = await page.$$eval('.items.normal .item.movies', items => {
-      return items.map(item => {
-        const title = item.querySelector('.poster a')?.href || null;
-        if (!title) return null;
+        return items.map(item => {
+            const title = item.querySelector('.poster a ')?.href || null;
+            const parts = title.split('/');
+            const id = parts[parts.length - 2];
+            const link = item.querySelector('.data h3 a')?.href || null;
+            const imgSrc = item.querySelector('.poster img')?.src || null;
+            const releaseDate = item.querySelector('.data span')?.textContent.trim() || null;
 
-        const parts = title.split('/');
-        const id = parts[parts.length - 2];
-        const link = item.querySelector('.data h3 a')?.href || null;
-        const imgSrc = item.querySelector('.poster img')?.src || null;
-        const releaseDate = item.querySelector('.data span')?.textContent.trim() || null;
-        const title2 = item.querySelector('.data h3 a')?.textContent.trim() || null;
+            const title2 = item.querySelector('.data h3 a')?.textContent.trim() || null;
 
-        return { id, title, link, imgSrc, releaseDate, title2 };
-      }).filter(peli => peli !== null);
+
+            return { id, title, link, imgSrc, releaseDate, title2 };
+        });
     });
 
-    // Actualizar los datos cacheados y el tiempo de caché
-    cachedData = peliculas;
-    lastCacheTime = Date.now();
+    await browser.close();
 
-    // Devolver los datos cacheados
-    res.json({ peliculas });
-  } catch (error) {
-    console.error('Error en el proceso de Puppeteer:', error);
+    res.json({
+        peliculas: peliculas.filter(peli => peli.title && peli.link),
+    });
+} catch (error) {
+    console.error(error);
     res.status(500).json({ error: 'Hubo un error al obtener los datos.' });
-  } finally {
-    if (browser) {
-      await browser.close(); // Asegurarse de cerrar el navegador en caso de error
-    }
+}finally {
+    await browser.close();
   }
 };
 
